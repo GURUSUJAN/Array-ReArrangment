60754c0a66d907e367f9c089cba737b3e3f84ae2
GURUSUJAN REDDY <94339643+GURUSUJAN@users.noreply.github.com>
2023-01-12 11:43:26 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,27 @@
+#include"iostream"
+using namespace std;
+int main()
+{
+    int n=0,i=0;
+    cout<<"size of an array: ";
+    cin>>n;
+    int ar[n],arr[n],a[n];
+    cout<<"Enter the elements of the array: "<<endl;
+    for(i=0;i<n;i++)
+    {
+        cin>>ar[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        arr[i]=ar[i];
+    }
+    cout<<"Rearranged array is: ";
+    for(i=0;i<n;i++)
+    {
+        a[i]=arr[ar[i]];
+    }
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<"\t";
+    }
+}
